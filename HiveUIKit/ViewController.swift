f8394239313de5a6ffb4746b44316d896fb5a0ff
Denis <Svetlakov>
2020-09-10 10:26:20 +1000

    Initial Commit

---
+++ HiveUIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HiveUIKit
+//
+//  Created by Denis Svetlakov on 10.09.2020.
+//  Copyright © 2020 Denis Svetlakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
